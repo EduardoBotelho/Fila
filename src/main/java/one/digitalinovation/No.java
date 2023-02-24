d928cea0054597f3c9bf587086a045f1dcbd4613
Eduardo <edu.botelho@yahoo.com>
2023-02-24 16:36:34 -0300

    Initial commit

---
+++ src/main/java/one/digitalinovation/No.java
@@ -0,0 +1,37 @@
+package one.digitalinovation;
+
+public class No<T> {
+    private Object object;
+    private  No<T> refNo;
+
+    public No(){ //contrutor padrao
+
+    }
+    public No(T object){
+        this.refNo = null;
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject( T object) {
+        this.object = object;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "object=" + object +
+                '}';
+    }
+}
